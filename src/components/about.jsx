ac395356a759c804619c5ea7d37dc59a149a1195
som <sombhud2637@gmail.com>
2023-07-24 22:39:01 +0530

    no

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,8 +11,7 @@ function about() {
         <h2>
             this is about page in this site 
         </h2>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas quis tempora commodi ratione veniam, facere sunt similique cupiditate consectetur ad quibusdam ut hic est praesentium expedita, amet accusamus? Beatae rerum deleniti quis dolorum soluta, dolores iusto fuga quod possimus maxime! Dolorem inventore at dicta cupiditate? Quos eos dolorem possimus sequi?</p>
-
+<p>sombhu das</p>
     </div>
   )
 }
